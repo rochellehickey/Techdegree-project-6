8a0b86387d53bcc7e49892e0da97697a836b79aa
Rochelle Hickey <rochellehickey@gmail.com>
2018-05-29 15:51:46 -0400

    Working on adding JS for video

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = express()
+    app.use(function (req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      next()
+    })
