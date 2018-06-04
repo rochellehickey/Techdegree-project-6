86e75eaaa1502e9669a6bf895d93cdee8474955d
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-04 14:46:14 -0400

    Trying to get the plugin to work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
-const captureCaptionText = document.querySelectorAll('span');
+//--------------VIDEO PLAYER CONTROLS--------------
 
-function hightlightCaptionText(captureCaptionText) {
-  for (let i = 0; i < hightlightCaptionText.length; i++) {
-
+// You can use either a string for the player ID (i.e., `player`),
+// or `document.querySelector()` for any selector
+var player = new MediaElementPlayer('player', {
+  pluginPath: "/plugins/mediaelement/",
+// When using `MediaElementPlayer`, an `instance` argument
+// is available in the `success` callback
+  success: function(mediaElement, originalNode, instance) {
+    // do things
+    alwaysShowControls: "true",
+    features: [playpause, current, progress, duration, tracks, volume, fullscreen],
   }
-}
+});
+
+
+//--------------HIGHLIGHTING TEXT--------------
+// const captureCaptionText = document.querySelectorAll('span');
+
+// function hightlightCaptionText(captureCaptionText) {
+//   for (let i = 0; i < captureCaptionText.length; i++) {
+//     captureCaptionText.style.color="orange";
+//   }
+// }
+
+
+// video.addEventListener('timeupdate', () => {
+
+// }
