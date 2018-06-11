b0e943ec4f7bcd0c44337212ffb42fec27c9c15f
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-10 21:15:21 -0400

    Adjusting the look of the plugin

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-//--------------VIDEO PLAYER CONTROLS--------------
-$('video').mediaelementplayer({
-  alwaysShowControls: true,
-  features: ['playpause', 'current', 'progress', 'duration', 'tracks', 'volume', 'fullscreen'],
-  stretching: 'responsive'
+$( document ).ready(function() {
+
+//-------------- VIDEO PLAYER CONTROLS --------------
+  $('video').mediaelementplayer({
+    alwaysShowControls: false,
+    features: ['playpause', 'current', 'progress', 'duration', 'tracks', 'volume', 'fullscreen'],
+    stretching: 'responsive'
+  });
+
+  //-------------- HIGHLIGHTING TEXT --------------
+
+  var videoTime = document.getElementsByTagName("video").currentTime;
+  var lines = document.querySelectorAll("span");
+
+  console.log(videoTime);
+
+  videoTime.addEventListener('timeupdate', function() {
+      for (var i = 0, l = lines.length; i < l; i++) {
+        if (videoTime >= lines.getAttribute("data-start") &&
+            videoTime <= lines.getAttribute("data-end")) {
+          lines[i].className = "current";
+        } else {
+          lines[i].className = "";
+        }
+      }
+  });
 });
-
-//--------------HIGHLIGHTING TEXT--------------
-// const captureCaptionText = document.querySelectorAll('span');
-
-// function hightlightCaptionText(captureCaptionText) {
-//   for (let i = 0; i < captureCaptionText.length; i++) {
-//     captureCaptionText.style.color="orange";
-//   }
-// }
-
-
-// video.addEventListener('timeupdate', () => {
-
-// }
-
-// video.addEventListener('timeupdate', ()=> {
-//     for (let i=0; i <text.length; i++){
-//       // if (video.currentTime >= text[i].dataset.start && video.currentTime <= text[i].dataset.end){
-//       if (video.currentTime >= text[i].getAttribute("data-start") && video.currentTime <= text[i].getAttribute("data-end")){
-//       text[i].style.color = 'orange';
-//   } else {
-//     text[i].style.color = 'black';
-//   }
-//  }
-// });
