52d5e386f0d78542ae11047e5633368cba3eb7f5
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-14 16:11:47 -0400

    Working on getting CC track to work

--- js/app.js
+++ js/app.js
@@ -6,19 +6,20 @@
   //   stretching: 'responsive'
   // });
 
-  // var lines = document.getElementsByTagName("span");
+  var lines = document.getElementsByTagName("span");
+  var myPlayer = document.getElementById("my-player");
 
-  // //-------------- HIGHLIGHTING TEXT --------------
+  //-------------- HIGHLIGHTING TEXT --------------
 
-  // myPlayer.addEventListener("timeupdate", function() {
-  //   console.log("Hi");
-  //     for (var i = 0, l = lines.length; i < l; i++) {
-  //       if (this >= lines.getAttribute("data-start") &&
-  //           this <= lines.getAttribute("data-end")) {
-  //         lines[i].className = "current";
-  //       } else {
-  //         lines[i].className = "";
-  //       }
-  //     }
-  // });
+  myPlayer.addEventListener("timeupdate", function() {
+    console.log("Hi");
+      for (var i = 0, l = lines.length; i < l; i++) {
+        if (this >= lines.getAttribute("data-start") &&
+            this <= lines.getAttribute("data-end")) {
+          lines[i].className = "current";
+        } else {
+          lines[i].className = "";
+        }
+      }
+  });
 
