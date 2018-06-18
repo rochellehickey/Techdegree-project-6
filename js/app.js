994f64897dbef339393711bec8c4ad512fac0d71
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-18 14:08:59 -0400

    Working on JS for highlighting

--- js/app.js
+++ js/app.js
@@ -1,21 +1,12 @@
-
-  // //-------------- VIDEO PLAYER CONTROLS --------------
-  // var myPlayer = new MediaElementPlayer('video', {
-  //   alwaysShowControls: false,
-  //   features: ['playpause', 'current', 'progress', 'duration', 'tracks', 'volume', 'fullscreen'],
-  //   stretching: 'responsive'
-  // });
-
-  var lines = document.getElementsByTagName("span");
-  var myPlayer = document.getElementById("my-player");
+I   var myPlayer = document.getElementById("my-player");
+  var lines = document.querySelectorAll(".transcript");
 
   //-------------- HIGHLIGHTING TEXT --------------
 
   myPlayer.addEventListener("timeupdate", function() {
-    console.log("Hi");
       for (var i = 0, l = lines.length; i < l; i++) {
-        if (this >= lines.getAttribute("data-start") &&
-            this <= lines.getAttribute("data-end")) {
+        if (myPlayer >= lines[i].getAttribute("data-start") &&
+            myPlayer <= lines[i].getAttribute("data-end")) {
           lines[i].className = "current";
         } else {
           lines[i].className = "";
@@ -23,3 +14,4 @@
       }
   });
 
+console.log('hi');
