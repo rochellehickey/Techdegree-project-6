4636176f48ac28897568a0a49d0675fd298908dc
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-18 20:34:25 -0400

    changed VAR to CONST and LET

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-var myPlayer = videojs("my-player");
-var lines = document.querySelectorAll(".transcript");
+const myPlayer = videojs("my-player");
+const lines = document.querySelectorAll(".transcript");
 
 //-------------- HIGHLIGHTING TEXT --------------
 
 myPlayer.on('timeupdate', function() {
-    for (var i = 0, l = lines.length; i < l; i++) {
-      var whereYouAt = this.currentTime();
-      var pDataStart = parseFloat(lines[i].getAttribute("data-start"));
-      var pDataEnd = parseFloat(lines[i].getAttribute("data-end"));
+    for (let i = 0, l = lines.length; i < l; i++) {
+      const whereYouAt = this.currentTime();
+      const pDataStart = parseFloat(lines[i].getAttribute("data-start"));
+      const pDataEnd = parseFloat(lines[i].getAttribute("data-end"));
 
       console.log(whereYouAt);
       // console.log(typeof parseFloat(lines[i].getAttribute("data-start")));
