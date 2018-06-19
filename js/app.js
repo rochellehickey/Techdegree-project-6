63b184413facf4ca976225dab3674e3443a74a61
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-18 20:32:22 -0400

    Got current time working

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
-I   var myPlayer = document.getElementById("my-player");
-  var lines = document.querySelectorAll(".transcript");
+var myPlayer = videojs("my-player");
+var lines = document.querySelectorAll(".transcript");
 
-  //-------------- HIGHLIGHTING TEXT --------------
+//-------------- HIGHLIGHTING TEXT --------------
 
-  myPlayer.addEventListener("timeupdate", function() {
-      for (var i = 0, l = lines.length; i < l; i++) {
-        if (myPlayer >= lines[i].getAttribute("data-start") &&
-            myPlayer <= lines[i].getAttribute("data-end")) {
-          lines[i].className = "current";
-        } else {
-          lines[i].className = "";
-        }
+myPlayer.on('timeupdate', function() {
+    for (var i = 0, l = lines.length; i < l; i++) {
+      var whereYouAt = this.currentTime();
+      var pDataStart = parseFloat(lines[i].getAttribute("data-start"));
+      var pDataEnd = parseFloat(lines[i].getAttribute("data-end"));
+
+      console.log(whereYouAt);
+      // console.log(typeof parseFloat(lines[i].getAttribute("data-start")));
+
+      if (myPlayer >= pDataStart && myPlayer <= pDataEnd) {
+        lines[i].className = "current";
+      } else {
+        lines[i].className = "";
       }
-  });
+    }
+});
+
 
 console.log('hi');
