4ad364a0c9624ba78d3360a5376561a787d9282e
Rochelle Hickey <rochellehickey@gmail.com>
2018-06-26 19:32:08 -0400

    Made it work for videos longer than 1 min

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 const myPlayer = videojs("my-player");
 const lines = document.querySelectorAll(".transcript");
-var i = 0;
 
 //-------------- HIGHLIGHTING TEXT --------------
 
 myPlayer.on('timeupdate', function() {
   for (var i = 0, l = lines.length; i < l; i++) {
     const whereYouAt = this.currentTime();
-    var pDataStart = parseFloat(lines[i].getAttribute("data-start").split(":")[2]);
-    var pDataEnd = parseFloat(lines[i].getAttribute("data-end").split(":")[2]);
+    var pDataStart = parseFloat(lines[i].getAttribute("data-start").replace(/:/g, ""));
+    var pDataEnd = parseFloat(lines[i].getAttribute("data-end").replace(/:/g, ""));
+
+    console.log(pDataEnd);
+    console.log(whereYouAt);
 
     if (whereYouAt > pDataStart && whereYouAt < pDataEnd) {
       lines[i].classList.add("current");
@@ -23,15 +25,12 @@ myPlayer.on('timeupdate', function() {
 // Listen for click event on page
 document.addEventListener('click', function(event) {
 
-  // Log the clicked element in the console
-  console.log(event.target);
-
   // IF clicked element is span with class transcript
   if (event.target.className === ('transcript')) {
 
     // Get data-start of span selected
     // Turn data-start string into a number
-    var pDataStart = parseFloat(event.target.getAttribute("data-start").split(":")[2]);
+    var pDataStart = parseFloat(event.target.getAttribute("data-start").replace(/:/g, ""));
 
     // Make current time of video data-start number
     var timeUpdate = myPlayer.currentTime(pDataStart);
